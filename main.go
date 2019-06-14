53bd6cb2d58008b8619edca620b7ced51649a302
João Pedro Hickmann <joaophickmann@outlook.com>
2019-06-13 22:32:35 -0300

    Ajustes

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	_ "github.com/mattn/go-sqlite3"
 	"golang.org/x/text/encoding/charmap"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strconv"
 	"time"
@@ -21,17 +25,123 @@ type Aula struct {
 }
 
 func main() {
-	html, err := getHtml()
+	db, err := configurarDB("/home/pedro/Projects/vaganatacao/horario.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_KEY"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	aulas, err := getAulas(html)
-	for _, aula := range aulas {
-		if aula.inicio.Hour() == 18 && aula.inicio.Minute() == 0 {
-			fmt.Printf("Dia: %s\nInicio: %s\nFim: %s\nTotal: %d\nDisponivel: %d\nInscritos: %d\n\n", aula.dia,
-				aula.inicio.Format("15:04"), aula.fim.Format("15:04"), aula.total, aula.disponivel, aula.inscritos)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	telegramChannel, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	esperaVagaChannel := make(chan []Aula)
+	go esperaVaga(esperaVagaChannel)
+
+	channelID, err := strconv.ParseInt(os.Getenv("TELEGRAM_CHANNEL_ID"), 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for true {
+		select {
+		case telegramUpdate := <-telegramChannel:
+			switch telegramUpdate.ChannelPost.Text {
+			case "/vagas":
+
+			case "/todos":
+				html, err := getHtml()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				msg := ""
+				aulas, err := getAulas(html)
+				for _, aula := range aulas {
+					msg = msg + fmt.Sprintf("Dia: %s\nInicio: %s\nFim: %s\nTotal: %d\nDisponivel: %d\nInscritos: %d\n\n", aula.dia,
+						aula.inicio.Format("15:04"), aula.fim.Format("15:04"), aula.total, aula.disponivel, aula.inscritos)
+				}
+
+			}
+		case aulasDisponiveis := <-esperaVagaChannel:
+			msg := ""
+			for _, aula := range aulasDisponiveis {
+				msg = msg + fmt.Sprintf("Dia: %s\nInicio: %s\nFim: %s\nTotal: %d\nDisponivel: %d\nInscritos: %d\n\n", aula.dia,
+					aula.inicio.Format("15:04"), aula.fim.Format("15:04"), aula.total, aula.disponivel, aula.inscritos)
+			}
+
+			_, err = bot.Send(tgbotapi.NewMessage(channelID, msg))
+			if err != nil {
+				log.Print(err)
+			}
 		}
+	}
+}
+
+func configurarDB(dataSourceName string) (db *sql.DB, err error) {
+	db, err = sql.Open("sqlite3", dataSourceName)
+	if err != nil {
+		return
+	}
+
+	sqlCreate := `
+		CREATE TABLE IF NOT EXISTS aulas
+		(
+			seq        INTEGER PRIMARY KEY AUTOINCREMENT,
+			dia        TEXT    NOT NULL,
+			inicio     DATE    NOT NULL,
+			fim        DATE    NOT NULL,
+			total      INTEGER NOT NULL,
+			disponivel INTEGER NOT NULL,
+			inscritos  INTEGER NOT NULL
+		)
+	`
+	_, errCreate := db.Exec(sqlCreate)
+	if errCreate != nil {
+		log.Print(errCreate)
+	}
+
+	return
+}
+
+func trataAulas(canal chan<- []Aula, aulas []Aula) {
+	aulasDisponiveis := []Aula{}
+	for _, aula := range aulas {
+		if aula.disponivel > 0 {
+			aulasDisponiveis = append(aulasDisponiveis, aula)
+		}
+	}
+	canal <- aulasDisponiveis
+}
+
+func esperaVaga(canal chan<- []Aula) {
+	for true {
+		html, err := getHtml()
+		if err != nil {
+			log.Print(err)
+			time.Sleep(10 * time.Minute)
+			continue
+		}
+
+		aulas, err := getAulas(html)
+		if err != nil {
+			log.Print(err)
+			time.Sleep(10 * time.Minute)
+			continue
+		}
+
+		trataAulas(canal, aulas)
+
+		time.Sleep(10 * time.Minute)
 	}
 }
 
